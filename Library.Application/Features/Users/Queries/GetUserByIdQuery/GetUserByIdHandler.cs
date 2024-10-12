77d63576608ece8cbab3005515827e46222e8242
Edilson Manjate <edilsonsh.manjate@gmail.com>
2024-10-12 12:01:07 +0200

    Add GetUserById query and handler
    Modified `Library.Application.csproj` to remove an `ItemGroup` for `Features\Users\Queries\GetUserByIdQuery`. Added `GetUserByIdHandler` class in `GetUserByIdHandler.cs` to handle `GetUserByIdQuery` requests using `IUserRepository` and `IMapper`. Introduced `GetUserByIdQuery` class in `GetUserByIdQuery.cs` to represent the query for fetching a user by ID.

---
+++ Library.Application/Features/Users/Queries/GetUserByIdQuery/GetUserByIdHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+
+using Library.Application.Common.Bases;
+using Library.Application.DTOs;
+using Library.Application.Repositories;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Features.Users.Queries.GetUserByIdQuery
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, BaseResponse<UserDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserByIdHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse<UserDto>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<UserDto>();
+
+            try
+            {
+                var user = await _userRepository.Get(request.UserId, cancellationToken);
+
+                if (user is not null)
+                {
+                    response.Data = _mapper.Map<UserDto>(user);
+                    response.succcess = true;
+                    response.Message = "Query succeed!";
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Library.Application/Features/Users/Queries/GetUserByIdQuery/GetUserByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using Library.Application.Common.Bases;
+using Library.Application.DTOs;
+
+using MediatR;
+
+namespace Library.Application.Features.Users.Queries.GetUserByIdQuery
+{
+    public class GetUserByIdQuery : IRequest<BaseResponse<UserDto>>
+    {
+        public Guid UserId { get; set; }
+
+    }
+}
